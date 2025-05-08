7d641ae7e4c88ef2c5a38c9e3ac6c025ebb0033a
soorya3101 <dsoorya3101@gmail.com>
2025-05-08 13:14:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,19 @@
-import streamlit as st
+import streamlit
+st.title(f"Question Paper")
+st.subheader(f"Grade: {Level}    PE: {periodic_evaluation}")
+st.markdown("---")
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+question_number = 1
+
+for section_title, q_type in [("I. Reading words 1", "three_letter_words"),
+                              ("II. Reading words 2", "four_letter_words"),
+                              ("III. Reading letters", "letters_1")]:
+    st.markdown(f"**{section_title}**")
+
+    subset = filtered[filtered["Question Type"] == q_type].sample(n=5, replace=True)
+    
+    for _, row in subset.iterrows():
+        st.write(f"{question_number}. {row['Question']}")
+        question_number += 1
+    
+    st.markdown("---")
